b311800dd33c55b261d85236e7eeed8a6d56dbd0
rafaelns17 <rafael.ns17@aluno.ifsc.edu.br>
2026-04-04 01:42:57 +0000

    ajustes de luz

--- scene0.js
+++ scene0.js
@@ -12,7 +12,7 @@ class scene0 extends Phaser.Scene {
   preload() {
     this.load.setPath("assets/");
 
-    this.load.audio("passos", "walkamongus.mp3"); 
+    this.load.audio("passos", "walkamongus.mp3");
 
     this.load.image("space", "assets-usados/space1.png");
 
@@ -70,61 +70,67 @@ class scene0 extends Phaser.Scene {
   }
 
   create() {
-
     this.passos = this.sound.add("passos", { loop: true, volume: 0.5 });
 
-    this.space = this.add.image(699, 1514, "space")
+    this.space = this.add.image(699, 1514, "space");
 
     this.anims.create({
       key: "gargantua-idle",
-      frames: this.anims.generateFrameNumbers("gargantua", { start: 0, end: 49 }),
+      frames: this.anims.generateFrameNumbers("gargantua", {
+        start: 0,
+        end: 49,
+      }),
       frameRate: 14,
       repeat: -1,
     });
 
-    this.gargantua = this.add.sprite(873, 950, "gargantua")
+    this.gargantua = this.add
+      .sprite(873, 950, "gargantua")
       .setScale(2)
       .play("gargantua-idle")
       .setPipeline("Light2D");
     this.gargantua.allowGravity = false;
-    
+
     this.tilemap = this.make.tilemap({ key: "todasfases" });
-    
+
     this.tilesetRemasterized = this.tilemap.addTilesetImage("remasterized");
-    this.tilesetRemasterizedEnfeites = this.tilemap.addTilesetImage("remasterizedEnfeites",);
+    this.tilesetRemasterizedEnfeites = this.tilemap.addTilesetImage(
+      "remasterizedEnfeites",
+    );
     this.tilesetUnnamed = this.tilemap.addTilesetImage("unnamed");
-    
-    
-    this.layerFundo = this.tilemap.createLayer("fundo", [
-      this.tilesetRemasterized,
-      this.tilesetRemasterizedEnfeites,
-    ]).setPipeline("Light2D") ;
-    this.layerEnfeites = this.tilemap.createLayer("enfeites", [
-      this.tilesetRemasterizedEnfeites,
-    ]).setPipeline("Light2D");
-    this.layerVidro = this.tilemap.createLayer("vidro", [
-      this.tilesetRemasterized,
-    ]).setPipeline("Light2D");
-    this.layerVidroH = this.tilemap.createLayer("vidroH", [
-      this.tilesetRemasterized,
-    ]).setPipeline("Light2D");
-    this.layerVidroh = this.tilemap.createLayer("vidroh", [
-      this.tilesetRemasterized,
-    ]).setPipeline("Light2D");
-    this.layerCadeira = this.tilemap.createLayer("cadeira", [
-      this.tilesetUnnamed,
-    ]).setPipeline("Light2D");
-    this.layerPiso = this.tilemap.createLayer("piso", [
-      this.tilesetRemasterized,
-    ]).setPipeline("Light2D");
-    
+
+    this.layerFundo = this.tilemap
+      .createLayer("fundo", [
+        this.tilesetRemasterized,
+        this.tilesetRemasterizedEnfeites,
+      ])
+      .setPipeline("Light2D");
+    this.layerEnfeites = this.tilemap
+      .createLayer("enfeites", [this.tilesetRemasterizedEnfeites])
+      .setPipeline("Light2D");
+    this.layerVidro = this.tilemap
+      .createLayer("vidro", [this.tilesetRemasterized])
+      .setPipeline("Light2D");
+    this.layerVidroH = this.tilemap
+      .createLayer("vidroH", [this.tilesetRemasterized])
+      .setPipeline("Light2D");
+    this.layerVidroh = this.tilemap
+      .createLayer("vidroh", [this.tilesetRemasterized])
+      .setPipeline("Light2D");
+    this.layerCadeira = this.tilemap
+      .createLayer("cadeira", [this.tilesetUnnamed])
+      .setPipeline("Light2D");
+    this.layerPiso = this.tilemap
+      .createLayer("piso", [this.tilesetRemasterized])
+      .setPipeline("Light2D");
+
     this.physics.world.setBounds(
       0,
       0,
       this.tilemap.widthInPixels,
       this.tilemap.heightInPixels,
     );
-    
+
     this.cameras.main.setBounds(
       0,
       0,
@@ -137,42 +143,42 @@ class scene0 extends Phaser.Scene {
       frameRate: 7,
       repeat: 0,
     });
-    
+
     this.anims.create({
       key: "walk-up",
       frames: this.anims.generateFrameNumbers("player", { start: 28, end: 35 }),
       frameRate: 10,
       repeat: -1,
     });
-    
+
     this.anims.create({
       key: "walk-left",
       frames: this.anims.generateFrameNumbers("player", { start: 40, end: 45 }),
       frameRate: 10,
       repeat: -1,
     });
-    
+
     this.anims.create({
       key: "walk-right",
       frames: this.anims.generateFrameNumbers("player", { start: 55, end: 60 }),
       frameRate: 10,
       repeat: -1,
     });
-    
+
     this.anims.create({
       key: "walk-down",
       frames: this.anims.generateFrameNumbers("player", { start: 44, end: 51 }),
       frameRate: 10,
       repeat: -1,
     });
-    
+
     this.anims.create({
       key: "idle",
       frames: this.anims.generateFrameNumbers("player", { start: 4, end: 5 }),
       frameRate: 3,
       repeat: -1,
     });
-    
+
     this.anims.create({
       key: "idleRight",
       frames: this.anims.generateFrameNumbers("player", { start: 6, end: 7 }),
@@ -186,14 +192,14 @@ class scene0 extends Phaser.Scene {
       frameRate: 3,
       repeat: -1,
     });
-    
+
     this.anims.create({
       key: "jump",
       frames: this.anims.generateFrameNumbers("player", { start: 27, end: 30 }),
       frameRate: 6,
       repeat: -1,
     });
-    
+
     this.anims.create({
       key: "jumpL",
       frames: this.anims.generateFrameNumbers("player", { start: 14, end: 17 }),
@@ -203,218 +209,214 @@ class scene0 extends Phaser.Scene {
 
     this.anims.create({
       key: "engrenagem-idle",
-      frames: this.anims.generateFrameNumbers("engrenagem", { start: 0, end: 1 }),
+      frames: this.anims.generateFrameNumbers("engrenagem", {
+        start: 0,
+        end: 1,
+      }),
       frameRate: 2,
       repeat: -1,
     });
-    
-    this.scoreText = this.add.text(100, 28, "Engrenagens: 0/5", {
-      fontSize: "32px",
-      fill: "#000",
-    }).setScrollFactor(0);
-    
-    this.engrenagem = this.physics.add.sprite(1138, 968, "engrenagem").setScale(0.3);
-    this.engrenagem.anims.play("engrenagem-idle", true)
-    .setImmovable(true)
-    .setPipeline("Light2D")
-    .body.allowGravity = false;
-    
+
+    this.scoreText = this.add
+      .text(100, 28, "Engrenagens: 0/5", {
+        fontSize: "32px",
+        fill: "#000",
+      })
+      .setScrollFactor(0);
+
+    this.engrenagem = this.physics.add
+      .sprite(1138, 968, "engrenagem")
+      .setScale(0.3);
+    this.engrenagem.anims
+      .play("engrenagem-idle", true)
+      .setImmovable(true)
+      .setPipeline("Light2D").body.allowGravity = false;
+
     this.cai = this.physics.add.sprite(500, 1150, "cai");
-    this.cai.setImmovable(true)
-    .setPipeline("Light2D")
-    .body.allowGravity = false;
-    
+    this.cai.setImmovable(true).setPipeline("Light2D").body.allowGravity =
+      false;
+
     this.door11 = this.physics.add.sprite(92, 1056, "door", 0);
-    this.door11.setPipeline("Light2D")
-    .body.allowGravity = false;
-    
+    this.door11.setPipeline("Light2D").body.allowGravity = false;
 
     this.door21 = this.physics.add.sprite(430, 897, "door", 0);
-    this.door21.setPipeline("Light2D")
-    .body.allowGravity = false;
-    
+    this.door21.setPipeline("Light2D").body.allowGravity = false;
+
     this.plataformG = this.physics.add.sprite(431, 930, "plataformG");
-    this.plataformG.setImmovable(true)
-    .setPipeline("Light2D")
-    .body.allowGravity = false;
-    
+    this.plataformG
+      .setImmovable(true)
+      .setPipeline("Light2D").body.allowGravity = false;
+
     //cria plataforma 1 e defne como imovível e de velocidade x = 100 além de fazê-la ignorar a gravidade
     this.plataform1 = this.physics.add.sprite(380, 1094, "plataform");
-    this.plataform1.setImmovable(true)
-    .setVelocityX(100)
-    .setPipeline("Light2D")
-    .body.allowGravity = false;
+    this.plataform1
+      .setImmovable(true)
+      .setVelocityX(100)
+      .setPipeline("Light2D").body.allowGravity = false;
 
     //num intervalo de 3400ms, inverte a velocidade da plataforma 1
     setInterval(() => {
       this.plataform1.setVelocityX(this.plataform1.body.velocity.x * -1);
     }, 3000);
-    
+
     this.plataform2 = this.physics.add.sprite(1140, 980, "plataform");
-    this.plataform2.setImmovable(true)
-    .setPipeline("Light2D")
-    .body.allowGravity = false;
-    
+    this.plataform2
+      .setImmovable(true)
+      .setPipeline("Light2D").body.allowGravity = false;
+
     this.plataform3 = this.physics.add.sprite(1050, 930, "plataform");
-    this.plataform3.setImmovable(true)
-    .setVelocityX(-150)
-    .setPipeline("Light2D")
-    .body.allowGravity = false;
-    
+    this.plataform3
+      .setImmovable(true)
+      .setVelocityX(-150)
+      .setPipeline("Light2D").body.allowGravity = false;
+
     setInterval(() => {
       this.plataform3.setVelocityX(this.plataform3.body.velocity.x * -1);
     }, 3490);
-    
-    this.player = this.physics.add.sprite(92, 1066, "player", 7).setScale(0.9);//fase1:92, 1052//fase2:108, 1834//
-    this.cameras.main.startFollow(this.player, false, 1, 0)
-    .zoom = 1.2;
-    this.cameras.main.scrollY = this.player.y - this.cameras.main.height / 2 - 120; // Ajuste para começar mais para cima (100 pixels acima do centro do jogador)
-    this.player.anims.play("idleRight", true)
-    .setPipeline("Light2D");
+
+    this.player = this.physics.add.sprite(92, 1066, "player", 7).setScale(0.9); //fase1:92, 1052//fase2:108, 1834//
+    this.cameras.main.startFollow(this.player, false, 1, 0).zoom = 1.2;
+    this.cameras.main.scrollY =
+      this.player.y - this.cameras.main.height / 2 - 120; // Ajuste para começar mais para cima (100 pixels acima do centro do jogador)
+    this.player.anims.play("idleRight", true).setPipeline("Light2D");
     this.doubleJump = false;
-    
-    this.lights.enable()
-    .setAmbientColor(0xE0F7FF) // 0xE0F7FF
-    .addLight(1040, 995, 80)
+
+    this.lights
+      .enable()
+      .setAmbientColor(0xe0f7ff) // 0xE0F7FF
+      .addLight(1040, 995, 80)
       .setIntensity(1)
       .setColor(0xffa500);
+    this.lights.addLight(921, 1032, 80).setIntensity(1).setColor(0xffa500);
+    this.lights.addLight(900, 1032, 80).setIntensity(1).setColor(0xffa500);
+    this.lights.addLight(860, 1020, 80).setIntensity(1).setColor(0xffa500);
+    this.lights.addLight(860, 950, 80).setIntensity(1).setColor(0xffa500);
+    this.lights.addLight(735, 1050, 50).setIntensity(1).setColor(0xffa500);
+    this.lights.addLight(784, 1025, 60).setIntensity(1).setColor(0xffa500);
     this.lights
-      .addLight(921, 1032, 80)
-      .setIntensity(1)
-        .setColor(0xffa500);
+      .addLight(this.door11.x, 1040, 40)
+      .setIntensity(1.5)
+      .setColor(0xff0000);
     this.lights
-      .addLight(900, 1032, 80)
-      .setIntensity(1)
-      .setColor(0xffa500);
+      .addLight(this.door21.x, 880, 40)
+      .setIntensity(1.5)
+      .setColor(0xff0000);
     this.lights
-      .addLight(860, 1020, 80)
-      .setIntensity(1)
-      .setColor(0xffa500);
-    this.lights
-      .addLight(860, 950, 80)
-      .setIntensity(1)
-      .setColor(0xffa500);
-    this.lights
-      .addLight(735, 1050, 50)
-      .setIntensity(1)
-      .setColor(0xffa500);
-    this.lights
-      .addLight(784, 1025, 60)
-      .setIntensity(1)
-      .setColor(0xffa500);
-    this.lights
-      .addLight(this.door11.x, 1035, 40)
-      .setIntensity(1)
-      .setColor(0xffa500);
+      .addLight(this.engrenagem.x, this.engrenagem.y, 40)
+      .setIntensity(1.5);
     /*this.lamp = this.lights
-    .addLight(this.player.x, this.player.y, 40)
-    .setIntensity(1.2);*/
-    
-    this.physics.add.overlap(this.player, this.engrenagem, this.collectEng, null, this);
-      this.physics.add.collider(this.engrenagem, this.plataform2);
-      this.physics.add.collider(this.player, this.layerPiso);
-      this.physics.add.collider(this.player, this.plataformG);
-      this.physics.add.collider(this.player, this.plataform1);
-      this.physics.add.collider(this.player, this.plataform2);
-      this.physics.add.collider(this.player, this.plataform3);
-      this.physics.add.overlap(this.player, this.cai, () => {
-        this.player.setPosition(92, 1066)
-        .setVelocity(0, 0);
-      });
+    .addLight(this.player.x, this.player.y, 35)
+    .setIntensity(1);*/
 
-      this.layerPiso.setCollisionByProperty({ collides: true });
+    this.physics.add.overlap(
+      this.player,
+      this.engrenagem,
+      this.collectEng,
+      null,
+      this,
+    );
+    this.physics.add.collider(this.engrenagem, this.plataform2);
+    this.physics.add.collider(this.player, this.layerPiso);
+    this.physics.add.collider(this.player, this.plataformG);
+    this.physics.add.collider(this.player, this.plataform1);
+    this.physics.add.collider(this.player, this.plataform2);
+    this.physics.add.collider(this.player, this.plataform3);
+    this.physics.add.overlap(this.player, this.cai, () => {
+      this.player.setPosition(92, 1066).setVelocity(0, 0);
+    });
 
-      // Texto de posição do player atualizado a cada segundo
-      this.positionText = this.add
-        .text(100, 50, "X: 0 Y: 0", {
-          fontSize: "18px",
-          fill: "#ffffff",
-          backgroundColor: "rgba(0,0,0,0.5)",
-          padding: { x: 6, y: 4 },
-        })
-        .setScrollFactor(0);
+    this.layerPiso.setCollisionByProperty({ collides: true });
 
-      this.time.addEvent({
-        delay: 1000,
-        loop: true,
-        callback: () => {
-          this.positionText.setText(
-            `X: ${Math.round(this.player.x)} Y: ${Math.round(this.player.y)}`,
-          );
-        },
-      });
-    
+    // Texto de posição do player atualizado a cada segundo
+    this.positionText = this.add
+      .text(100, 50, "X: 0 Y: 0", {
+        fontSize: "18px",
+        fill: "#ffffff",
+        backgroundColor: "rgba(0,0,0,0.5)",
+        padding: { x: 6, y: 4 },
+      })
+      .setScrollFactor(0);
+
+    this.time.addEvent({
+      delay: 1000,
+      loop: true,
+      callback: () => {
+        this.positionText.setText(
+          `X: ${Math.round(this.player.x)} Y: ${Math.round(this.player.y)}`,
+        );
+      },
+    });
   }
 
-    update() {
+  update() {
+    /*this.lamp.x = this.player.x;
+    this.lamp.y = this.player.y;*/
 
-      //this.lamp.x = this.player.x;
-      //this.lamp.y = this.player.y;
-      
-      if (this.input.gamepad && this.input.gamepad.total > 0) {
-        const pad = this.input.gamepad.getPad(0);
+    if (this.input.gamepad && this.input.gamepad.total > 0) {
+      const pad = this.input.gamepad.getPad(0);
 
-        if (pad.axes.length > 0) {
-          const horizontal = pad.axes[0].getValue();
+      if (pad.axes.length > 0) {
+        const horizontal = pad.axes[0].getValue();
 
-          if (horizontal > 0) {
-            this.player.setVelocityX(200);
-            this.direction = true;
-            if (this.player.body.velocity.y === 0) {
-              this.player.anims.play("walk-right", true);
-              this.passos.play();
-            }
-          } else if (horizontal < 0) {
-            this.player.setVelocityX(-200)
-            this.direction = false;
-            if (this.player.body.velocity.y === 0) {
-              this.player.anims.play("walk-left", true);
-              this.passos.play();
-            }
-          } else {
-            this.player.setVelocityX(0);
+        if (horizontal > 0) {
+          this.player.setVelocityX(200);
+          this.direction = true;
+          if (this.player.body.velocity.y === 0) {
+            this.player.anims.play("walk-right", true);
+            this.passos.play();
           }
-        }
-        
-        if (this.player.body.blocked.down) {
-          this.doubleJump = false;
-          if (pad.X)
-            this.player.setVelocityY(-300)
-        }
-        
-      
-        if (this.player.body.blocked.left || this.player.body.blocked.right) {
-          if (this.player.body.velocity.x != 0 && pad.X && !this.doubleJump) {
-            this.player.setVelocityY(-415);
-            this.doubleJump = true;
+        } else if (horizontal < 0) {
+          this.player.setVelocityX(-200);
+          this.direction = false;
+          if (this.player.body.velocity.y === 0) {
+            this.player.anims.play("walk-left", true);
+            this.passos.play();
           }
+        } else {
+          this.player.setVelocityX(0);
         }
       }
-      if (this.direction === true &&
-        this.player.body.velocity.x === 0 &&
-        this.player.body.velocity.y === 0 &&
-        (this.player.body.blocked.down || this.player.body.blocked.up)) {
-        this.player.anims.play("idleRight", true);
-      } else if (this.direction === false &&
-        this.player.body.velocity.x === 0 &&
-        this.player.body.velocity.y === 0 &&
-        (this.player.body.blocked.down || this.player.body.blocked.up)) {
-        this.player.anims.play("idleLeft", true);
+
+      if (this.player.body.blocked.down) {
+        this.doubleJump = false;
+        if (pad.X) this.player.setVelocityY(-300);
       }
-    
-      if (this.player.body.velocity.y < 0 && this.direction === true) {
-        this.player.anims.play("jump", true);
-      } else if (this.player.body.velocity.y < 0 && this.direction === false) {
-        this.player.anims.play("jumpL", true);
+
+      if (this.player.body.blocked.left || this.player.body.blocked.right) {
+        if (this.player.body.velocity.x != 0 && pad.X && !this.doubleJump) {
+          this.player.setVelocityY(-415);
+          this.doubleJump = true;
+        }
       }
     }
-  
-    collectEng(player, engrenagem){
-      engrenagem.disableBody(true, true);
-  
-      this.score += 1;
-      this.scoreText.setText("Engrenagens: " + this.score + "/5");
-  
+    if (
+      this.direction === true &&
+      this.player.body.velocity.x === 0 &&
+      this.player.body.velocity.y === 0 &&
+      (this.player.body.blocked.down || this.player.body.blocked.up)
+    ) {
+      this.player.anims.play("idleRight", true);
+    } else if (
+      this.direction === false &&
+      this.player.body.velocity.x === 0 &&
+      this.player.body.velocity.y === 0 &&
+      (this.player.body.blocked.down || this.player.body.blocked.up)
+    ) {
+      this.player.anims.play("idleLeft", true);
+    }
+
+    if (this.player.body.velocity.y < 0 && this.direction === true) {
+      this.player.anims.play("jump", true);
+    } else if (this.player.body.velocity.y < 0 && this.direction === false) {
+      this.player.anims.play("jumpL", true);
     }
   }
-  export default scene0;
+
+  collectEng(player, engrenagem) {
+    engrenagem.disableBody(true, true);
+
+    this.score += 1;
+    this.scoreText.setText("Engrenagens: " + this.score + "/5");
+  }
+}
+export default scene0;
